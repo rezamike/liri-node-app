e5812628dbf225307cc06ef7ba5703cfcab5c62e
rezamike <michael.sanaiha@gmail.com>
2018-09-13 21:15:30 -0700

    start spotify attempt to call certain elements within node-api

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+var fs = require("fs");
 var request = require("request")
 require("dotenv").config();
 var Spotify = require("node-spotify-api");
@@ -27,6 +28,33 @@ switch (command) {
 
     break;
     case "spotify-this-song" :
+
+    fs.appendFile("random.txt", input, function(err) {
+
+        // If an error was experienced we will log it.
+        if (err) {
+          console.log(err);
+        }
+      
+        // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+        else {
+          console.log("Content Added!");
+        }
+      
+      });
+
+    // request(Spotify, function (error, response, event) {
+
+    //     var conInfo = JSON.parse(event);
+
+    //     if (!error && response.statusCode === 200) {
+    //         console.log((conInfo[0].lineup).toString().replace(",", ", "))
+    //         console.log("Venue Name: " + conInfo[0].venue.name);
+    //         console.log("Venue Location: " + conInfo[0].venue.city);
+    //         console.log("Concert Date: " + moment(conInfo[0].datetime).format("MM/DD/YYYY"));
+    //     };
+
+    // });
     
     break;
     case "movie-this" :
