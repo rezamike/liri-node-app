4fb120f945afdb433bb43e149762b76a5ed6cbcd
rezamike <michael.sanaiha@gmail.com>
2018-09-15 13:32:04 -0700

    connect spotify arguement - no default yet

--- liri.js
+++ liri.js
@@ -7,7 +7,7 @@ var moment = require("moment");
 
 var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
-var input = process.argv[3];
+var input = process.argv.splice(3).join(" ");
 
 switch (command) {
     case "concert-this" :
@@ -34,7 +34,11 @@ switch (command) {
           return console.log('Error occurred: ' + err);
         }
        
-      console.log(JSON.parse(data.tracks.items.artists[0])); 
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("Song: " + data.tracks.items[0].name);
+      console.log("Preview: " + data.tracks.items[0].preview_url);
+      console.log("Album: " + data.tracks.items[0].album.name);
+        
       });
 
     // request(Spotify, function (error, response, event) {
@@ -52,6 +56,19 @@ switch (command) {
     
     break;
     case "movie-this" :
+
+    request("http://www.omdbapi.com/?apikey=trilogy&s=" + input, function (error, response, event) {
+
+        var conInfo = JSON.parse(event);
+
+        if (!error && response.statusCode === 200) {
+            console.log((conInfo[0].lineup).toString().replace(",", ", "))
+            console.log("Venue Name: " + conInfo[0].venue.name);
+            console.log("Venue Location: " + conInfo[0].venue.city);
+            console.log("Concert Date: " + moment(conInfo[0].datetime).format("MM/DD/YYYY"));
+        };
+
+    });
     
     break;
     case "do-what-it-says" :
