73d058d624cbea8828f7c0badc131259841c780e
rezamike <michael.sanaiha@gmail.com>
2018-09-15 19:22:47 -0700

    finish main functions - missing default values and general function

--- liri.js
+++ liri.js
@@ -4,74 +4,199 @@ require("dotenv").config();
 var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
 var moment = require("moment");
+var inquirer = require("inquirer");
 
 var spotify = new Spotify(keys.spotify);
-var command = process.argv[2];
-var input = process.argv.splice(3).join(" ");
 
-switch (command) {
-    case "concert-this" :
+// var input = process.argv.splice(3).join(" ");
 
-    request("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp", function (error, response, event) {
-
-        var conInfo = JSON.parse(event);
-
-        if (!error && response.statusCode === 200) {
-            console.log((conInfo[0].lineup).toString().replace(",", ", "))
-            console.log("Venue Name: " + conInfo[0].venue.name);
-            console.log("Venue Location: " + conInfo[0].venue.city);
-            console.log("Concert Date: " + moment(conInfo[0].datetime).format("MM/DD/YYYY"));
-        };
-
-    });
-
-
-    break;
-    case "spotify-this-song" :
-
-    spotify.search({ type: "track", query: input, limit: 1 }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
+inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "What would you like to look up?",
+            choices: ["Concert Information", "Song Information", "Movie Information", "General", "Creator"],
+            name: "main"
         }
-       
-      console.log("Artist: " + data.tracks.items[0].artists[0].name);
-      console.log("Song: " + data.tracks.items[0].name);
-      console.log("Preview: " + data.tracks.items[0].preview_url);
-      console.log("Album: " + data.tracks.items[0].album.name);
-        
-      });
+    ])
+    .then(function (response) {
+        var command = response.main;
 
-    // request(Spotify, function (error, response, event) {
+        switch (command) {
+            case "Concert Information":
 
-    //     var conInfo = JSON.parse(event);
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: "Please type your Artist for the search: ",
+                            name: "concert"
+                        }
+                    ])
+                    .then(function (response) {
+                        var concertInput = response.concert;
+                        concertInput = (typeof concertInput !== 'undefined') ? concertInput : "beyonce";
 
-    //     if (!error && response.statusCode === 200) {
-    //         console.log((conInfo[0].lineup).toString().replace(",", ", "))
-    //         console.log("Venue Name: " + conInfo[0].venue.name);
-    //         console.log("Venue Location: " + conInfo[0].venue.city);
-    //         console.log("Concert Date: " + moment(conInfo[0].datetime).format("MM/DD/YYYY"));
-    //     };
+                        request("https://rest.bandsintown.com/artists/" + concertInput + "/events?app_id=codingbootcamp", function (error, response, event) {
 
-    // });
-    
-    break;
-    case "movie-this" :
+                            var conInfo = JSON.parse(event);
 
-    request("http://www.omdbapi.com/?apikey=trilogy&s=" + input, function (error, response, event) {
+                            if (!error && response.statusCode === 200) {
+                                console.log((conInfo[0].lineup).toString().replace(",", ", "))
+                                console.log("Venue Name: " + conInfo[0].venue.name);
+                                console.log("Venue Location: " + conInfo[0].venue.city);
+                                console.log("Concert Date: " + moment(conInfo[0].datetime).format("MM/DD/YYYY"));
+                            };
 
-        var conInfo = JSON.parse(event);
+                        });
+                    })
 
-        if (!error && response.statusCode === 200) {
-            console.log((conInfo[0].lineup).toString().replace(",", ", "))
-            console.log("Venue Name: " + conInfo[0].venue.name);
-            console.log("Venue Location: " + conInfo[0].venue.city);
-            console.log("Concert Date: " + moment(conInfo[0].datetime).format("MM/DD/YYYY"));
-        };
+                break;
+            case "Song Information":
 
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: "Please type your Song for the search: ",
+                            name: "song"
+                        }
+                    ])
+                    .then(function (response) {
+                        var songInput = response.song;
+
+                        spotify.search({ type: "track", query: songInput, limit: 1 }, function (err, data) {
+                            if (err) {
+                                return console.log('Error occurred: ' + err);
+                            }
+
+                            console.log("Artist: " + data.tracks.items[0].artists[0].name);
+                            console.log("Song: " + data.tracks.items[0].name);
+                            console.log("Preview: " + data.tracks.items[0].preview_url);
+                            console.log("Album: " + data.tracks.items[0].album.name);
+
+                        });
+                    })
+
+                break;
+            case "Movie Information":
+
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: "Please type your Movie for the search: ",
+                            name: "movie"
+                        }
+                    ])
+                    .then(function (response) {
+                        var movieInput = response.movie;
+
+                        request("http://www.omdbapi.com/?apikey=trilogy&t=" + movieInput, function (error, response, event) {
+
+                            var movieInfo = JSON.parse(event);
+
+                            if (!error && response.statusCode === 200) {
+                                console.log("Title: " + movieInfo.Title)
+                                console.log("Year: " + movieInfo.Year);
+                                console.log("IMBD Rating: " + movieInfo.imdbRating);
+                                console.log("Rotten Tomatoes Rating: " + movieInfo.Ratings[1].Value);
+                                console.log("Country: " + movieInfo.Country);
+                                console.log("Language: " + movieInfo.Language);
+                                console.log("Plot: " + movieInfo.Plot);
+                                console.log("Actors: " + movieInfo.Actors);
+                            };
+
+                        });
+                    })
+
+                break;
+            case "General":
+
+                fs.readFile("random.txt", "utf8", function (error, data) {
+
+                    if (error) {
+                        return console.log(error);
+                    }
+
+                    console.log(data);
+                    var dataArr = data.split(",");
+                    console.log(dataArr);
+
+                });
+
+                break;
+            case "Creator":
+
+                console.log("@@@@@@@@@@@@@@@@@@@@@@@@@&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((%@%&******************************///////////////////(((");
+                console.log("@@@@@@@@@@@@@@@@@@@@@@@@@&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@((@@%&*******************************/////////////////((((");
+                console.log("@@@@@@@@@@@@@@@@@@@@@@@@@&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(#@@%&********************************///*//////////////((");
+                console.log("@@@@@@@@@@@@@@@@@@@@@@@@&&&&@@@@@@@@@@@@@@@@@@@@&&&&%%%&@@@@@(%@@%%**********************************/*//////////////((");
+                console.log("@@@@@@@&@&@@@@@@@@@@@@@@&%&&@@@@@@@@@@@@@@@@%%%%%&&@&&&&&&&&&(@@@%%,********************************/*/*/////////////(/");
+                console.log("@@@@&&&&&&&&&&&&&&@@@@@@&%%&@@@@@@@@@@@@@&%&&&&&@@@&@@&&&&&@&%&&%**,**********************************///////////////");
+                console.log("@@@&%####%%%%&&&&&&&&&&&&%%&&@@@@@@@@@@@&&&&&&&&&&&&@@@@@@&@@@&&&%#,,,********************************/*///////////////");
+                console.log("&&&%#(((((%%%%%%%%%%%%&&%%%%&@@@@@@@@@@&@@&@&&&&&@@&&&&@@@@@@@@&&@@#,,,**********************************//////////////");
+                console.log("%%%%%(((((#########%%%&&%%%%&@@@@@@@@@&&@@&&&&&%&&&&&&%&&@@&@@&&@@@@*,,***********************************/////////////");
+                console.log("#%%%%%############%%%%&&%%%&@@@@@@@@&&@&&&&@&&&&&&&&@@@@@&@@&&&&&@@@/,,,,***********************************///////////");
+                console.log("####%%%%####%%%%%%%%%&&&%###%%&&&&&&%@@&@@@@@@@@&&&@@@@@@@@&@@@@@@@@@*,,,,*********************************//*/////////");
+                console.log("%%%%%%%%%%%%%%%%%%&&&&&&%######%%%%%&@@@@@&@@&&%%%%%&&%%%%&&@@@@@@@@#,,,,,,*,***,****************************//////////");
+                console.log("%%%%%%&&&&&&&&&&&&&&&&&####%%%%%%%@@@%((####((//////////(#&@@@@@@@(,,,,*****,*******************************/////////");
+                console.log("&&&&&&&&&&&&&&&&&&&&&@&%#####%%%%%%%@@@(////**************/(#&@@@@@@(,***************************************//*///////");
+                console.log("&&&&&&&&&&&&&&&&&&&&@@&%####%&&&&&(*@@%(//***,,,,,**********(%@@@@@@(*****************************************/*///////");
+                console.log("&&&&&&&&&&&&&&&&&%#((#%%###%%&&&&(,,/@((///*,,,,,,,,,,,***///(&@@@@(*********************************************//////");
+                console.log("%%#(//((#%%%%%&%/,...,/%&%%&&@@@@/   @/(%&@@**,,**(##%%%#((/(&@@##**********************************************/////");
+                console.log("%*.     .*#&&&&&(***(#%#%%&&&&&*  *&//#&%%&%%(//#%%%#(((#((//&@@@***********************************************/*///");
+                console.log("%/,,,,,,,*(%&&%%*,   .,(#####(((((//(%/**/(##(((**/###@@&(///&@@%*************************************************///");
+                console.log("%*.       ,(####(/////*..,.       .. .//******//,**/*/////**//#@&@&**************************************************//");
+                console.log("%(,....,,*/((####(,.           .      &/***,,//*,**/******///(@%#@************///***********************************//*");
+                console.log(",,,..,*//((((/.               ..      %%/****/*,,**//****//(#&*#************//**************************************/");
+                console.log("///*********   .    .         .    .  (@%****/#//(##****//(%@@*,,/***********/*//**************************************");
+                console.log("***********                   ..      .@@#&@@@@@@@@&%##//#@@@...*..  .,*****////***************************************");
+                console.log("**********              ..    ..   .   %%@%%(#####%%&@@&%@@#....(.  . ..**/*////***************************************");
+                console.log("*********                .    .    .   .@&%(#(((####&@@%.,...(... ...   */////***************************************");
+                console.log("********                 ..  ..    .    /&@&&@&&%&@@@(,...**,,...,..    .,////***************,,**********************");
+                console.log("*******                  ..  ..          (%%%%@@@@@@@/,.%@@@,.....,..   ,   ...****************************************");
+                console.log("******,        .       . ..  ....      *,,,*(/*/#(**,,.,&@@(.........  .        ...************************************");
+                console.log("******                 . ..  ....     .*,,.../*,,*,,,...,,..............            . ..*******************************");
+                console.log("**//*/                .....  .. .     ........(*,*,,.,,,.......,.........          .  ..  .....*,**********************");
+                console.log("*////.              .......  .. .      .......,,,,,,,,... ....,...................... .. ....  .... ..........*********");
+                console.log("//(/,                ......  .           . .,,...,.,...  ....,..... ...................... ........,........,....******");
+                console.log("((/*                  .,...               ............   ...,........,.,,.,,,,,.......... .........................,***");
+                console.log("(//                    ....  .           ..    ......   . .,..........,,.,,,,,....,,,...,.......,,....   .. ........,**");
+                console.log("//.                     ,,. ...               .  ... .....,..........,*.,,,,,...,,,,,,.......,(####(((((,. . ........,*");
+                console.log("/*                     .,,......       . ..  ...... .....,..........,,.*,,,,..,,,,,,,,*((#####((((((/((((((,...........");
+                console.log("#                      .*,....       ..  .  ......    ..,.........,*,*,,,.,,,,**(######(/(((/((((((((((######/.......,,");
+                console.log(",                    ..*,,..       .,.  .   ....     ..,,........****,,,.,,/#(//******//*///(((((((##((((((((###*,,,**/");
+                console.log("                     ./,,*.      ..,..       ..    ..,,,,......,****,,,,*/////***,*((##(((((((((##((((((####%%%&&&%/*/#");
+                console.log("                    ./*,*,,,,,*/*,,,...... ...   ...,,,,,,...,/***///**//*****//**/(##(((##%%%#%%%%#%%%%%%&@@@&&&&&&%&&");
+                console.log("                   .(/,****///**,,,,....   ..  ,...****.....//*/*///*/////////**//(&&&&&&&&@@@@@@@@@@@@@@@@&&&&&&&%%%%%");
+                console.log("                   ,(/*///////**,,,.......,..,*.. ...,.,,*///*****/**///*/(////#%%&&&&&&&&&&&&&&&@&&@@@@@&&&&&&&&&&%%%%");
+                console.log("                    ##((((((/**,,,.........,**,......,*(//*///*,*//*,*//**////#&&&&&&&&&&&&&&&&@@@@&&&&&&&&&&&&&&&&&&&&");
+                console.log("                   (&%#((((//**.........,*//,,.....*/((//(((/**(/#(/**((/**///%&&&&&&&&&&&&&&&&@&&&&&&&&&&&&&&&&&&&&&&&");
+                console.log("                  ,##((%((//////*,,,,,**//,...,,,,//////(#*,*((##%#//*(%///&&&&@@@@&@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+                console.log("                  ////((*////*,,,******/*,,...,,**///*/(#(**(((/(%(//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+                console.log("                 //*,/*,,,***,*///////*,,,,..,,,*///((/((##(////#&&%(/*(&&&&&&&&&&&&&&&&&&&%&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+                console.log("               .***/*,,,***//(*******,,..,,..,**//((/(((##&%##%%%%%%%(//(%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%&&&&");
+                console.log("*             .,.*(****//////,,,.,..,,,,,*/((((#(((((/((#%%%%%%%#%%####((#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                console.log(".              .,(////***,,,......,,,,,**//(((////**(((######################################%%%%%%%%%%%%%%%%%%%%%%%%%%");
+                console.log("                ,*,,,,,,,,.......,,,,*/((/////****((((#####((((((((((((#############################################%%%");
+                console.log("...          . .,**,,,,,,,,,,,,,,,*/////******//(((((((((((((((((((((((((((((((((((####################################");
+                console.log(",.............,,,,,,,,*********///*******/////(((((((((((((((((((((((((((((((((((((((((((((((##########################");
+                console.log("/**/*,,,,.,,,,,,************//******////////((((((((((((((((((((//(/((((((((((((((((((((((((((((#((####################");
+                console.log("//////****************////**///////////////((((((((((((((((((((/(////((((((((((((((((((((((((##########################");
+                console.log("(//////**************//////////////////(((((((((((((((((((((((((((((((((((((((((((((((############################%%%%%");
+                console.log("(((//////////////////////////////////((((((((((((((((((((((((((((((((((((((((######################%%#%%%%%%%%%%%%%%%%%");
+                console.log("((((((//////////////////////(((((((((((((((((((((((((#(#######((#(((#########################%%%%%%%%%%%%%%%%%%%%%%%%%&");
+
+                request("http://artii.herokuapp.com/make?text=Michael Sanaiha&font=trek", function (error, response, body) {
+
+                if (!error && response.statusCode === 200) {
+
+                    console.log(body);
+                }
+            });
+
+            break;
+        }
     });
-    
-    break;
-    case "do-what-it-says" :
-    
-    break;
-}
+
